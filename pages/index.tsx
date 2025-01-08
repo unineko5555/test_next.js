033083d5ebfe85703be0f80b1cb7e00c6b876d36
unineko5555 <yuking04170417@gmail.com>
2025-01-08 14:36:32 +0900

    ✨ useEffectの導入

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import { Footer } from "@/components/Footer";
 import { Main } from "@/components/Main";
 import { Header } from "@/components/Header";
+import { use, useEffect } from "react";
+import { useCallback } from "react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -14,9 +16,30 @@ const geistMono = Geist_Mono({
 });
 
 export default function Home() {
+  const foo = 1;
+
+  const handleClick = useCallback((e) => {
+    console.log(e.target.href);
+    e.preventDefault();
+    alert(foo);
+  }, []);
+
+  useEffect(() => {
+    console.log("マウント時");
+    document.body.style.backgroundColor = "lightblue";
+
+    return () => {
+      console.log("アンマウント時");
+      document.body.style.backgroundColor = "";
+    };
+  }, []);
+
   return (
     <div className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}>
       <Header />
+      <a href="/about" onClick={handleClick}>
+        ボタン
+      </a>
       <Main page="index" />
       <Footer />
     </div>
