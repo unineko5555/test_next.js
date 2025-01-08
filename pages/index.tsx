b2e2e244e7c257f710ae1e6dd25c1701c3ff7623
unineko5555 <yuking04170417@gmail.com>
2025-01-09 00:06:02 +0900

    ✨ useStateで真偽値と文字列について

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,31 +16,43 @@ const geistMono = Geist_Mono({
 
 export default function Home() {
   const [count, setCount] = useState(1);
+  const [text, setText] = useState("");
+  const [isShow, setIsShow] = useState(true);
+
 
   const handleClick = useCallback(() => {
-    console.log(count);
     if (count < 10) {
       setCount((count) => count + 1);
     }
   }, [count]);
+
+  const handleDisplay = useCallback(() => {
+    setIsShow((isShow) => !isShow); 
+  }, []);
+
+  const handleChange = useCallback((e) => {
+    if (e.target.value.length > 5) {
+      alert("5文字以内で入力してください");
+      return;
+    }
+    setText(e.target.value.trim());
+  }, []);
   
   useEffect(() => {
-    console.log(`マウント時: ${count}`);
     document.body.style.backgroundColor = "lightblue";
 
     return () => {
-      console.log(`アンマウント時: ${count}`);
       document.body.style.backgroundColor = "";
     };
   }, [count]);
 
-  console.log(count);
-
   return (
     <div className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}>
       <Header />
-      <h1>{count}</h1>
+      {isShow ? <h1>{count}</h1> : null}
       <button onClick={handleClick}>ボタン</button>
+      <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
+      <input type="text" value={text} onChange={handleChange} />
       <Main page="index" />
       <Footer />
     </div>
